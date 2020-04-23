a848b72fa4a5bad090d79609937c1004e3cbc87c
Brad <brad@viziworx.com>
2020-04-23 11:04:46 -0400

    removes axios import, adds fetchShows function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
 
 import { formatSeasons } from "./utils/formatSeasons";
 
 import Episodes from "./components/Episodes";
+import fetchShow from "./api/fetchShows"
 import "./styles.css";
 
 export default function App() {
@@ -15,17 +15,11 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
+    fetchShow()
+        .then(response => {
+          setShow(response.data);
+          setSeasons(formatSeasons(response.data._embedded.episodes));
         });
-    };
-    fetchShow();
   }, []);
 
   const handleSelect = e => {
